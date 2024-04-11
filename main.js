0fa01a9edc0555a778f2492139cb15775032f797
AntranigSameen <166274087+AntranigSameen@users.noreply.github.com>
2024-04-10 21:20:47 -0700

    Fixed nav buttons for landscape and portrait
    Everything is functioning appropriately except for portrait dots

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
+
+
 var slideIndex = 1;
+var portraitSlideIndex = 1; // New index for portrait slides
 showSlides(slideIndex);
 
-function plusSlides(n){
-    showSlides(slideIndex +=n);
+function plusSlides(n, isPortrait){
+    if (isPortrait) {
+        showPortraitSlides(portraitSlideIndex += n);
+    } else {
+        showSlides(slideIndex += n);
+    }
 }
 
 function currentSlide(n){
     showSlides(slideIndex = n);
 }
 
+function currentSlides(n, isPortrait){
+    if (isPortrait) {
+        showPortraitSlides(portraitSlideIndex = n);
+    } else {
+        showSlides(slideIndex = n);
+    }
+}
+
 function showSlides(n){
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    if(n > slides.length) { slideIndex = 1}
-    if(n <1) { slideIndex = slides.length}
+    if(n > slides.length) { slideIndex = 1; }
+    if(n < 1) { slideIndex = slides.length; }
     for (i = 0; i < slides.length; i++){
         slides[i].style.display = "none";
     }
@@ -24,3 +39,20 @@ function showSlides(n){
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active"; 
 }
+
+function showPortraitSlides(n){
+    var i;
+    var portraitSlides = document.getElementsByClassName("mySlidesPort");
+    var dots = document.getElementsByClassName("dotPort");
+    if(n > portraitSlides.length) { portraitSlideIndex = 1; }
+    if(n < 1) { portraitSlideIndex = portraitSlides.length; }
+    for (i = 0; i < portraitSlides.length; i++){
+        portraitSlides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    portraitSlides[portraitSlideIndex - 1].style.display = "block";
+    dots[portraitSlideIndex - 1].className += " active"; 
+}
+

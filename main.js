93da90c45521f9ca74d459b6c151f4e97db6c056
AntranigSameen <166274087+AntranigSameen@users.noreply.github.com>
2024-04-12 10:09:00 -0700

    Fixed all slides and img nav to be responsive and functional

--- main.js
+++ main.js
@@ -10,6 +10,13 @@ document.addEventListener('DOMContentLoaded', function () {
 document.addEventListener('DOMContentLoaded', function () {
     showNatureSlides(1); // Display the first landscape slide initially
 });
+document.addEventListener('DOMContentLoaded', function () {
+    showPortraitSlidesRandom(1); // Display the first portrait slide initially
+});
+document.addEventListener('DOMContentLoaded', function () {
+    showRandomSlides(1); // Display the first landscape slide initially
+});
+
 
 
 var slideIndex = 1;
@@ -116,4 +123,57 @@ function currentNatureSlides(n, isPortrait) {
     } else {
         showNatureSlides(slideIndex = n);
     }
+}
+
+// FUNCTIONS FOR RANDOM SLIDES
+
+var randomSlideIndex = 1;
+var portraitRandomSlideIndex = 1; // New index for portrait slides in the random section
+
+function plusRandomSlides(n, isPortrait) {
+    if (isPortrait) {
+        showPortraitSlidesRandom(portraitRandomSlideIndex += n);
+    } else {
+        showRandomSlides(randomSlideIndex += n);
+    }
+}
+
+function currentRandomSlides(n, isPortrait) {
+    if (isPortrait) {
+        showPortraitSlidesRandom(portraitRandomSlideIndex = n);
+    } else {
+        showRandomSlides(randomSlideIndex = n);
+    }
+}
+
+function showRandomSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlidesRandom");
+    var dots = document.getElementsByClassName("dotRandom");
+    if (n > slides.length) { randomSlideIndex = 1; }
+    if (n < 1) { randomSlideIndex = slides.length; }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[randomSlideIndex - 1].style.display = "block";
+    dots[randomSlideIndex - 1].className += " active";
+}
+
+function showPortraitSlidesRandom(n) {
+    var i;
+    var portraitSlides = document.getElementsByClassName("mySlidesPortRandom");
+    var dots = document.getElementsByClassName("dotPortRandom");
+    if (n > portraitSlides.length) { portraitRandomSlideIndex = 1; }
+    if (n < 1) { portraitRandomSlideIndex = portraitSlides.length; }
+    for (i = 0; i < portraitSlides.length; i++) {
+        portraitSlides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    portraitSlides[portraitRandomSlideIndex - 1].style.display = "block";
+    dots[portraitRandomSlideIndex - 1].className += " active";
 }
